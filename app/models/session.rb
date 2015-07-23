ba76edaf612f68cd5e70fd62f7128b77239c7f3c
SAkshat <akshat@vinsol.com>
2015-07-23 10:42:11 +0530

    Added validations

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,20 @@
 class Session < ActiveRecord::Base
   belongs_to :event
-  # has_one :speaker, as :user
+
+  validates :name, :topic, :location, presence: true
+  validates :description, length: { minimum: 50, maximum: 250 }
+  validate :date_during_event_duration
+  validate :end_time_greater_than_start_time
+
+  private
+
+    def date_during_event_duration
+      unless date > event.start_date.to_date && date < event.end_date.to_date
+        errors[:date] << "must be with event date"
+      end
+    end
+
+    def end_time_greater_than_start_time
+      errors[:end_time] << "should be more than start time" if start_time > end_time
+    end
 end

7676b02c1b6b8e414848e6149c39595705c88c93
SAkshat <akshat@vinsol.com>
2015-08-12 18:06:03 +0530

    Minor changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < Devise::SessionsController
 
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-    self.current_user = @user
+    auth = request.env['omniauth.auth']
     redirect_to root_path
   end
 
   protected
 
-  def auth_hash
-    request.env['omniauth.auth']
-  end
+    def auth_hash
+      request.env['omniauth.auth']
+    end
 
 end

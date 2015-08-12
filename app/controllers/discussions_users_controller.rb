d206fcdf3afbf6b2970fe47d707dfb4ce3774b30
SAkshat <akshat@vinsol.com>
2015-08-12 19:15:27 +0530

    Made changes as suggested

--- app/controllers/discussions_users_controller.rb
+++ app/controllers/discussions_users_controller.rb
@@ -1,8 +1,9 @@
 class DiscussionsUsersController < ApplicationController
 
   def create
-    # [TODO - S] Should check if the discussion with this id exist.
-    if current_user.discussions_users.create(discussion_id: params[:id])
+    # [DONE TODO - S] Should check if the discussion with this id exist.
+    discussion = Discussion.find_by(id: params[:id])
+    if discussion && current_user.discussions_users.create(discussion_id: discussion.id)
       redirect_to :back, notice: "You have successfully RSVP'd to this discussion"
     else
       redirect_to :back, flash: { error: "Couldn't RSVP to the discussion" }
@@ -10,8 +11,9 @@ class DiscussionsUsersController < ApplicationController
   end
 
   def destroy
-    # [TODO - S] nil.destroy will raise exception.
-    if current_user.discussions_users.find_by(discussion_id: params[:id]).destroy
+    # [DONE TODO - S] nil.destroy will raise exception.
+    discussion_user = current_user.discussions_users.find_by(discussion_id: params[:id])
+    if discussion_user && discussion_user.destroy
       redirect_to :back, notice: 'You opted out of this discussion'
     else
       redirect_to :back, flash: { error: "Couldn't opt out of the discussion" }
--- app/models/contact_detail.rb
+++ app/models/contact_detail.rb
@@ -2,6 +2,6 @@ class ContactDetail < ActiveRecord::Base
   belongs_to :contactable, polymorphic: true
   validates :email, :phone_number, presence: true
   validates :email, format: { with: EMAIL_REGEXP }, allow_blank: true
-  # [TODO - S] phone_number is a string field
-  validates :phone_number, numericality: { only_integer: true }, allow_blank: true
+  # [DONE TODO - S] phone_number is a string field
+  validates :phone_number, format: { with: /\A[0-9]{10}\z/ }, allow_blank: true
 end

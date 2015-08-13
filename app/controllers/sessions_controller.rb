abc7654251305e93e226ebc447afd3f469ba8e97
SAkshat <akshat@vinsol.com>
2015-08-13 16:42:42 +0530

    Basic sign in achieved

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
-class SessionsController < Devise::SessionsController
+class SessionsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:create]
 
   def create
     auth = request.env['omniauth.auth']
-    redirect_to root_path
+    user = User.create_with(nickname: auth[:info][:nickname], image_path: auth[:info][:image], twitter_url: auth[:info][:urls][:Twitter]).find_or_create_by(uid: auth[:uid])
+    user.save(validate: false)
+    sign_in_and_redirect user
   end
 
   protected

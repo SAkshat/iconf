b0f3797579230eed6d57533e83b7754c871e301a
SAkshat <akshat@vinsol.com>
2015-08-14 10:34:24 +0530

    Removed unwanted code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id] ) if session[:user_id]
-  end
-
-  helper_method :current_user
 end

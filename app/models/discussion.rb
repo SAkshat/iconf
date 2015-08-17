89a496432fbd82dd62d1bb8e531fe18710f98a32
SAkshat <akshat@vinsol.com>
2015-08-17 18:46:30 +0530

    Made changes as suggested

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -15,7 +15,6 @@ class Discussion < ActiveRecord::Base
   validate :end_time_greater_than_start_time
   # [DONE TODO DOUBT - S] This should not be a validation. Why??
   # Implement using callbacks
-  validate :can_discussion_be_enabled, on: [:enable, :disable]
 
   scope :enabled, -> { where(enabled: true) }
 
@@ -23,12 +22,6 @@ class Discussion < ActiveRecord::Base
     start_time, end_time = event.start_time.to_date, event.end_time.to_date
     unless date >= start_time && date <= end_time
       errors[:date] << "must be within the event duration [#{ start_time } -- #{ end_time }]"
-    end
-  end
-
-  def can_discussion_be_enabled
-    if !(event.enabled? && creator.enabled?)
-      errors[:base] << 'cannot be enabled'
     end
   end
 

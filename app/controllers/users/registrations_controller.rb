60446707a95406ab9345e17e62143feb663d67ed
SAkshat <akshat@vinsol.com>
2015-08-28 19:19:56 +0530

    med designation ot title and made minor css and layout changes to make it look more presentable

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
-    params.require(:user).permit(:name, :designation, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :title, :email, :password, :password_confirmation)
   end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
     user.enabled
   end
 
+  def get_user_name(user)
+    if user.nickname?
+      return user.nickname
+    else
+      return user.title + ' ' + user.name
+    end
+  end
+
 end

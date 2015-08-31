4043bfdde42f876b8559a8820cd347f729dcc746
SAkshat <akshat@vinsol.com>
2015-08-31 16:23:24 +0530

    Renamed some functions

--- app/models/discussions_user.rb
+++ app/models/discussions_user.rb
@@ -4,14 +4,14 @@ class DiscussionsUser < ActiveRecord::Base
   belongs_to :discussion
 
   after_create :queue_user_for_discussion_reminder
-  before_destroy :remove_reminder_email_from_queue
+  after_destroy :remove_user_from_disucssion_reminder_queue
 
   def queue_user_for_discussion_reminder
     delayed_job = UserMailer.delay(run_at: reminder_time(discussion)).reminder_email(discussion, user)
     DiscussionsUser.where(user: user, discussion: discussion).first.update(delayed_job_id: delayed_job.id)
   end
 
-  def remove_reminder_email_from_queue
+  def remove_user_from_disucssion_reminder_queue
     delayed_job = Delayed::Job.find_by(id: delayed_job_id)
     if delayed_job
       delayed_job.delete

b6e929ce916133e16871271ada80674f07833c2c
SAkshat <akshat@vinsol.com>
2015-09-29 16:05:42 +0530

    Replaced constant Users with User

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationsController < Devise::RegistrationsController
+class User::RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit(:name, :title, :email, :password, :password_confirmation)

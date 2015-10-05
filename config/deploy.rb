9d34ca2f2827166891cba9aff357ac5587dc9dda
SAkshat <akshat@vinsol.com>
2015-10-05 20:10:35 +0530

    Added capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application,     'iconf'
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
+set :branch, fetch(:branch, 'sesisons')
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false

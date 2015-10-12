49a019dceb77e708ad414aedf09ed293df83a348
SAkshat <akshat@vinsol.com>
2015-10-12 15:13:48 +0530

    Fixed routing error. Upon login, was mistakenly refering t a controller which doesn't exist

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
 
